365d146900a777738fe456b7a0c79dffdf25a7a6
JHankins09 <Hankins.JamesB@gmail.com>
2019-07-22 12:02:23 -0400

    Scaffold groups to API

---
+++ app/serializers/group_serializer.rb
@@ -0,0 +1,3 @@
+class GroupSerializer < ActiveModel::Serializer
+  attributes :id, :Name, :Type, :Intro
+end
---
+++ db/migrate/20190722155520_create_groups.rb
@@ -0,0 +1,11 @@
+class CreateGroups < ActiveRecord::Migration[5.2]
+  def change
+    create_table :groups do |t|
+      t.string :Name, null: false
+      t.string :Type, null: false
+      t.string :Intro, null: false
+
+      t.timestamps
+    end
+  end
+end
